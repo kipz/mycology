e4487dccf1941d9fd48c79fde4c20749c20e27d4
James Carnegie <james.carnegie@datadoghq.com>
2025-11-17 21:21:26 +0000

    rendering fixed

--- src/main/java/org/potato/mycology/MycologyModClient.java
+++ src/main/java/org/potato/mycology/MycologyModClient.java
@@ -1,14 +1,23 @@
 package org.potato.mycology;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
+import org.potato.mycology.registry.ModBlocks;
 
 public class MycologyModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		MycologyMod.LOGGER.info("Initializing Mycology Mod Client");
 
-		// Client-specific initialization will go here
-		// (e.g., rendering, GUI, key bindings)
+		// Register render layers for transparent mushroom blocks
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CHANTERELLE_MUSHROOM, RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PORCINI_MUSHROOM, RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FIELD_MUSHROOM_BLOCK, RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.OYSTER_MUSHROOM_BLOCK, RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PARASOL_MUSHROOM_BLOCK, RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FLY_AGARIC_MUSHROOM, RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.DEATH_CAP_MUSHROOM, RenderType.cutout());
 
 		MycologyMod.LOGGER.info("Mycology Mod Client initialized successfully");
 	}
