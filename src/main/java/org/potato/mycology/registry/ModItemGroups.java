c6b9708946ed66cd2cbe005c55df1604bcd610ce
James Carnegie <james.carnegie@datadoghq.com>
2025-11-17 23:33:05 +0000

    register all mushrooms in a loop

--- src/main/java/org/potato/mycology/registry/ModItemGroups.java
+++ src/main/java/org/potato/mycology/registry/ModItemGroups.java
@@ -6,8 +6,10 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.potato.mycology.MycologyMod;
+import org.potato.mycology.item.mushroom.MushroomItem;
 
 public class ModItemGroups {
     public static final CreativeModeTab MYCOLOGY_TAB = Registry.register(
@@ -17,42 +19,15 @@ public class ModItemGroups {
                     .title(Component.translatable("itemGroup.mycology.mycology_tab"))
                     .icon(() -> new ItemStack(ModItems.CHANTERELLE))
                     .displayItems((parameters, output) -> {
-                        // Edible mushrooms
-                        output.accept(ModItems.CHANTERELLE);
-                        output.accept(ModItems.PORCINI);
-                        output.accept(ModItems.FIELD_MUSHROOM);
-                        output.accept(ModItems.OYSTER_MUSHROOM);
-                        output.accept(ModItems.PARASOL_MUSHROOM);
-                        output.accept(ModItems.GIANT_PUFFBALL);
-                        output.accept(ModItems.BAY_BOLETE);
-                        output.accept(ModItems.CHICKEN_OF_THE_WOODS);
-                        output.accept(ModItems.HEDGEHOG_MUSHROOM);
-                        output.accept(ModItems.WOOD_BLEWIT);
-                        output.accept(ModItems.SHAGGY_INK_CAP);
-                        output.accept(ModItems.HORN_OF_PLENTY);
-                        output.accept(ModItems.BEEFSTEAK_FUNGUS);
-                        output.accept(ModItems.SAFFRON_MILK_CAP);
-                        output.accept(ModItems.DRYADS_SADDLE);
-                        output.accept(ModItems.JELLY_EAR);
-                        output.accept(ModItems.LIONS_MANE);
-
-                        // Medicinal mushrooms
-                        output.accept(ModItems.TURKEY_TAIL);
-                        output.accept(ModItems.BIRCH_POLYPORE);
-                        output.accept(ModItems.HOOF_FUNGUS);
-                        output.accept(ModItems.ARTISTS_BRACKET);
-                        output.accept(ModItems.CHAGA);
-                        output.accept(ModItems.REISHI);
-
-                        // Poisonous mushrooms
-                        output.accept(ModItems.DEATH_CAP);
-                        output.accept(ModItems.DESTROYING_ANGEL);
-                        output.accept(ModItems.DEADLY_FIBRECAP);
-                        output.accept(ModItems.DEADLY_WEBCAP);
-                        output.accept(ModItems.YELLOW_STAINER);
-                        output.accept(ModItems.FLY_AGARIC);
-                        output.accept(ModItems.PANTHER_CAP);
-                        output.accept(ModItems.FALSE_CHANTERELLE);
+                        // Automatically add all registered mushroom items from our mod
+                        for (Item item : BuiltInRegistries.ITEM) {
+                            if (item instanceof MushroomItem) {
+                                ResourceLocation itemId = BuiltInRegistries.ITEM.getKey(item);
+                                if (itemId.getNamespace().equals(MycologyMod.MOD_ID)) {
+                                    output.accept(item);
+                                }
+                            }
+                        }
                     })
                     .build()
     );
